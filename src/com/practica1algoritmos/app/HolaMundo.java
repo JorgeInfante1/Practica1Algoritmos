f1eb45510e292d0303c2439d685f851f1db71dfb
JorgeInfante1 <jorge_infante90@hotmail.com>
2019-10-10 12:53:33 -0500

    First commit of my project Java. Is the typical Hello world of the first application of Java

---
+++ src/com/practica1algoritmos/app/HolaMundo.java
@@ -0,0 +1,17 @@
+package com.practica1algoritmos.app;
+
+import javax.swing.JOptionPane;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		String cadena;
+		// TODO Auto-generated method stub
+       cadena=JOptionPane.showInputDialog("Hola como estas?: ");
+		JOptionPane.showMessageDialog(null,"Tu estas: "+cadena);
+      // System.out.println("Hola Mundo");
+       
+       //System.out.println("caballo");
+	}
+
+}
